766b231e467cd5ecfb39bf874d20c2b4428adfdb
Milan-imca <milan.imca22@gmail.com>
2024-09-22 11:14:19 +0530

    final-commit

---
+++ components/Header.tsx
@@ -0,0 +1,16 @@
+import { cn } from '@/lib/utils'
+import Image from 'next/image'
+import Link from 'next/link'
+
+const Header = ({ children, className }: HeaderProps) => {
+  return (
+    <div className={cn("header", className)}>
+      <Link href='/' className="md:flex-1">
+        <p className='text-3xl'>DocHub</p>
+      </Link>
+      {children}
+    </div>
+  )
+}
+
+export default Header
